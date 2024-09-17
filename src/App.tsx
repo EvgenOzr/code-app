7f39de4114f5c2bb01f110c6625d5be781b9eb03
EvgenOzr <131382405+EvgenOzr@users.noreply.github.com>
2024-09-17 23:30:55 +0500

    edit README

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {ChangeEvent, useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const [text, setText] = useState('')
+	const [codeText, setCodeText] = useState('')
+
+	const codingText = () => {
+		let newArrText;
+		let lastArray = ''; 
+		if(text) {
+			newArrText = text.split('')
+			let counter = 0;
+			for( let i = 0; i < newArrText.length; i ++){
+				if (newArrText[i] === newArrText[i + 1]) {
+					counter++;
+				} else {
+					if(counter) {
+						lastArray += counter + 1 + newArrText[i]
+						counter = 0;
+					}else {
+						lastArray += newArrText[i]
+					}
+				}
+			}
+			setCodeText(lastArray)
+		}
+	}
+
+	const onChangeText = (e:ChangeEvent<HTMLInputElement> ) => {
+		const newText = e.target.value;
+		setText(newText)
+	}
+
+	const onClipboard = () => {
+		navigator.clipboard.writeText(codeText)
+			.then(() => {
+				alert('Текст скопирован в буфер обмена')
+			})
+			.catch((err) => {
+				alert('Произошла ошибка!')
+			})
+	}
+
+	return (
+		<div className='codeForm'>
+			<div className='inner'>
+				<h3>Кодирование строки</h3>
+				<input className='source' type="text" onChange={onChangeText}/>
+				<button type="button" className="btn btn-primary" onClick={codingText}>Кодировать</button>
+				<div className='result'>Результат: {codeText}</div>
+				<button type="button" className="btn btn-primary" onClick={onClipboard}>Скопировать в буфер обмена</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
